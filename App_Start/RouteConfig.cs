91c4b58ef647417d55ffaee1c33926f3c00e699e
Prateek Gupta <prateek@Prateeks-MBP.localdomain>
2019-04-22 11:32:54 -0700

    Created merchant sample in c#

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace mechant_sample_csharp
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute(
+                "Default",
+                "{action}/{id}",
+                new {controller = "Application", action = "Index", id = UrlParameter.Optional}
+            );
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mechant_sample_csharp.MvcApplication" Language="C#" %>
